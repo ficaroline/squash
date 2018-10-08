9d79105465c08adecf16447546c091972ba63a82
Zejjari <p249216@generali.ch>
2018-10-08 18:48:11 +0200

    Top Paddle

--- src/de/openhpi/capstone1/counter/starter/TheApp.java
+++ src/de/openhpi/capstone1/counter/starter/TheApp.java
@@ -1,8 +1,6 @@
 package de.openhpi.capstone1.counter.starter;
 
-import de.openhpi.capstone1.counter.builder.GUIComponent;
 import de.openhpi.capstone1.counter.builder.InteractiveComponent;
-import de.openhpi.capstone1.counter.builder.InteractiveCounterBuilder;
 import processing.core.PApplet;
 
 public class TheApp extends PApplet {
@@ -10,23 +8,16 @@ public class TheApp extends PApplet {
 	InteractiveComponent interactiveCounter;
 
 	int hochz, hochz2;
-//	float bx;
 	int boxH = 20;
 	int boxW = 100;
-	
-	float bx = map(mouseX, 0, width, 0, width - boxW);
-	
+	int x2;
 	boolean overBox = false;
 	boolean locked = false;
 	float xOffset = (float) 0.0;
 	float yOffset = (float) 0.0;
-
 	int rad = 10; // Width of the shape
 	float xpos, ypos; // Starting position of shape
-
 	float xspeed = (float) 2.5; // Speed of the shape
-//	float yspeed = (float) 2.5; // Speed of the shape
-
 	int xdirection = 1; // Left or Right
 	int ydirection = 1; // Top to Bottom
 
@@ -38,68 +29,57 @@ public class TheApp extends PApplet {
 	@Override
 	public void setup() { // setup() runs once
 		noStroke();
-
 		frameRate(30);
-//		InteractiveCounterBuilder builder = new InteractiveCounterBuilder();
-//		GUIComponent.construct(this, builder);
-//		interactiveCounter = builder.getComponent();
-
-		bx = width / 2 - boxW /2;
-
-//		rectMode(RADIUS);
-
 		ellipseMode(RADIUS);
 		// Set the starting position of the shape
 		xpos = width / 2;
 		ypos = height / 2;
+	}
 
+	/**
+	 * Tastaturbedienung oberes Paddle
+	 * 
+	 * */	
+	public void keyPressed() {
+		if (key == CODED) {
+			if (keyCode == LEFT && x2>0) {
+				x2 -= 15;
+			} else if (keyCode == RIGHT && x2<width-boxW) {
+				x2 += 15;
+			} 
+		} 
 	}
 
 	@Override
 	public void draw() { // draw() loops forever, until stopped
-		
-		
-		
-		xspeed =  (float) (Math.round(xspeed * 100) / 100.0);  		
 		background(204);
 		
+		xspeed =  (float) (Math.round(xspeed * 100) / 100.0);  	
+		// Score Anzeige
 		hochz++;
 		textSize(15);
 		fill(0, 102, 153);
-		text("Score: " +hochz2 + " Speed: " + xspeed, 10, 30); 
-		
+		text("Score: " +hochz2 + " Speed: " + xspeed, 10, 15 ); 		
 		if (ypos != 350) hochz2 = (int) hochz/10;				
 		if (hochz2%2==0 && ypos != 350) xspeed+=0.01;
-				
+			
 		fill(255);
-//		interactiveCounter.update();
-
 		float  x1 = map(mouseX, 0, width, 0, width - boxW);
-		rect(x1, 270, boxW, boxH);
-	
+		rect(x2, 30, boxW, boxH);		// Top Paddle
+		rect(x1, 270, boxW, boxH); 		// Bottom Paddle
+		
 		// Update the position of the shape
 		xpos = xpos + (xspeed * xdirection);
 		ypos = ypos + (xspeed * ydirection);
-//		ypos = ypos + (yspeed * ydirection);
-
-		if (ypos > 270 - rad && x1 <= xpos && xpos <= x1 + 100) {
-			ydirection *= -1;	
-		}
-		
-		if (xpos > 300 - rad || xpos < rad ) {
-			xdirection *= -1;
-		}
-		if (ypos < rad) {
-			ydirection *= -1;
-		}
-		if (ypos > 300 - rad ) {
+		if (ypos > 270 - rad && x1 <= xpos && xpos <= x1 + 100) ydirection *= -1;	// untere Seite auf dem unteren Paddle
+		if (ypos < 50 + rad && x2 <= xpos && xpos <= x2 + 100) ydirection *= -1;	// Obere Seite auf dem oberen Paddle		
+		if (xpos > 300 - rad || xpos < rad ) 	xdirection *= -1;                   // Ränder  
+		if (ypos > 300 - rad || ypos < 30 ) {										// Ausserhalb
 			xpos = 350;
 			ypos = 350;
 		}
 
 		ellipse(xpos, ypos, rad, rad);
-		
-
 	}
 
 }
